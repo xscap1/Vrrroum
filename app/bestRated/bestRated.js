a3dd30aa4d2cd09a9d3f829228774f29ab4cd017
xscap1 <poujoltheo@gmail.com>
2023-12-19 17:01:00 +0100

    [app] Created bestRated screen

---
+++ app/bestRated/bestRated.js
@@ -0,0 +1,26 @@
+import { SafeAreaView, ScrollView, View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import commonStyles from "../../styles/common";
+
+const BestRated = () => {
+    const router = useRouter()
+  
+    return (
+      <View style={commonStyles.body}>
+        <SafeAreaView>
+          <Stack.Screen
+            options={{
+              headerStyle: commonStyles.header,
+              headerShadowVisible: false,
+              headerTitle: "",
+            }}
+          /> 
+
+          <Text>BEST RATED ECRAN OUI</Text>
+
+        </SafeAreaView>
+      </View>
+    );
+  };
+  
+  export default BestRated;

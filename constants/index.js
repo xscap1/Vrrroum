13029119663d738e8e5051c30b6c7a5b6ff16e1e
xscap1 <poujoltheo@gmail.com>
2023-12-20 13:40:24 +0100

    [constants] Re imported icons file to constants index

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 import images from "./images";
-import icons from "./icons";
+import icons from "./icons"
 import { COLORS, SIZES, SHADOWS } from "./theme";
 
 export { images, icons, COLORS, SIZES, SHADOWS };

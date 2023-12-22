5f430b0807e42816a81eb98f9d655bb9c41139c4
xscap1 <poujoltheo@gmail.com>
2023-12-22 12:29:24 +0100

    [constants] Added icons constants

--- constants/icons.js
+++ constants/icons.js
@@ -1,3 +1,13 @@
+import home  from "../assets/icons/home.png"
+import home_selected  from "../assets/icons/home_selected.png"
+import barcode  from "../assets/icons/barcode.png"
+import barcode_selected  from "../assets/icons/barcode_selected.png"
+import user  from "../assets/icons/user.png"
+import user_selected  from "../assets/icons/user_selected.png"
 import motoroil from "../assets/icons/motor_oil.png"
+import motoroil_selected from "../assets/icons/motor_oil_selected.png"
 
-export default { motoroil };
+export default { motoroil, motoroil_selected,
+                 home, home_selected,
+                 barcode, barcode_selected,
+                 user, user_selected};

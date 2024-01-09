8f9ec97c1f9be984907898e57fea6898fd22ed4d
xscap1 <poujoltheo@gmail.com>
2024-01-09 16:29:37 +0100

    [component] Created OfferCard component

---
+++ components/common/cards/offers/OfferCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import commonStyles from "../../../../styles/common";
+import styles from "./offerCardStyles";
+import { COLORS, SIZES, icons } from "../../../../constants";
+
+const OfferCard = ({image}) => {
+    return (
+        <View style={styles.wrapper}>
+            <Image source={image} style={styles.image} />
+        </View>
+    );
+};
+
+export default OfferCard;

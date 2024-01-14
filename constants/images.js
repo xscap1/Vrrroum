38f4d803755b0132b45aa676e810c2628d9c770f
xscap1 <poujoltheo@gmail.com>
2024-01-14 10:14:57 +0100

    [constants] Exported offers images as constants

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import offer1 from "../assets/images/offers/offer_1.jpeg"
+import offer2 from "../assets/images/offers/offer_2.jpeg"
+import offer3 from "../assets/images/offers/offer_3.jpeg"
+
+export default {offer1, offer2, offer3};

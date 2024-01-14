8a7e13fa63aba34a1a47ffe84c4b49193908b2c2
xscap1 <poujoltheo@gmail.com>
2024-01-14 10:16:15 +0100

    [app] Added Offer component to home screen

--- app/home.js
+++ app/home.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { SafeAreaView, ScrollView, View, Text } from "react-native";
 import { Stack, useRouter, Link } from "expo-router";
 import Trendings from "../components/home/Trendings";
@@ -5,11 +6,18 @@ import BestRated from "../components/home/BestRated";
 import Categories from "../components/home/Categories";
 import Offers from "../components/home/Offers";
 import commonStyles from "../styles/common";
-import Footer from "../components/common/footer/Footer"
-
+import Footer from "../components/common/footer/Footer";
+import { SearchBar } from '@rneui/themed';
+import { COLORS } from "../constants";
 
 const Home = () => {
     const router = useRouter()
+
+    const [search, setSearch] = useState("");
+
+    const updateSearch = (search) => {
+      setSearch(search);
+    };
 
     return (
       <View style={commonStyles.body}>
@@ -22,9 +30,20 @@ const Home = () => {
                 headerTitle: "",
                 headerBackVisible : false
             }}
-          /> 
-
+          />
+          
           <ScrollView style={{height:'90%'}}showsVerticalScrollIndicator={false}>
+            <View style={{paddingLeft: 0.02 * ww, paddingRight: 0.02 * ww,}}>
+              <SearchBar
+                  placeholder="Rechercher un produit"
+                  onChangeText={updateSearch}
+                  value={search}
+                  platform="ios"
+                  containerStyle={{backgroundColor: COLORS.background}}
+                  inputContainerStyle={{backgroundColor: COLORS.darkgray}}
+                  inputStyle={{backgroundColor: COLORS.darkgray, color: COLORS.lightwhite}}
+              />
+            </View>
             <Offers />
             <Trendings />
             <BestRated />

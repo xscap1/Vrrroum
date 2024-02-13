4dc2ebc1cc0487edcd111cbc767a729c58026f16
xscap1 <poujoltheo@gmail.com>
2024-02-13 21:05:14 +0100

    [styles] Added new style to add flame icon to preview card style

--- components/common/cards/previewCard/previewCardStyles.js
+++ components/common/cards/previewCard/previewCardStyles.js
@@ -26,6 +26,18 @@ const styles = StyleSheet.create({
         alignSelf: "flex-start",
     },
 
+    scanContainer: {
+        flexDirection: "row",
+        backgroundColor: COLORS.background,
+        borderColor: COLORS.lightgreen,
+        borderWidth: 1,
+        borderRadius: 100,
+        paddingVertical: 5,
+        paddingHorizontal: 8,
+        justifyContent: "center",
+        alignSelf: "flex-start",
+    },
+
     scanWrap: {
         backgroundColor: COLORS.background,
         borderColor: COLORS.lightgreen,
@@ -41,6 +53,12 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         width : '100%',
         height: '100%'
+    },
+
+    icon: {
+        width: 15,
+        height: 15,
+        margin: 1
     },
 
     brandWrap: {

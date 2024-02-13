06419a88467dce38bafaef43f98d28180e783836
xscap1 <poujoltheo@gmail.com>
2024-02-13 22:21:36 +0100

    [app] Display products in bestRated and trends screens

--- app/(tabs)/home/bestRated.js
+++ app/(tabs)/home/bestRated.js
@@ -1,26 +1,43 @@
-import { SafeAreaView, ScrollView, View, Text } from "react-native";
+import { SafeAreaView, ScrollView, View, Text, ActivityIndicator } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import commonStyles from "../../../styles/common";
+import ListedProducts from "../../../components/home/ListedProducts";
+import React, { useEffect, useState } from 'react';
 
 const BestRated = () => {
-    const router = useRouter()
-  
-    return (
-      <View style={commonStyles.body}>
-        <SafeAreaView>
-          <Stack.Screen
-            options={{
-              headerStyle: commonStyles.header,
-              headerShadowVisible: false,
-              headerTitle: "",
-            }}
-          /> 
+  const router = useRouter();
 
-          <Text>BEST RATED ECRAN OUI</Text>
+  const api = require('../../../api/api');
 
-        </SafeAreaView>
-      </View>
-    );
-  };
-  
-  export default BestRated;
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    api.getBestRatedFromApi(setData, setLoading);
+  }, []);
+
+  return (
+    <View style={commonStyles.body}>
+      <SafeAreaView style={commonStyles.flexSafeArea}>
+        <Stack.Screen
+          options={{
+            headerStyle: commonStyles.header,
+            headerShadowVisible: false,
+            headerTitle: "",
+          }}
+        />
+        <View style={commonStyles.flexContainer}>
+          <Text style={commonStyles.heading}>Les mieux notés</Text>
+          <Text style={commonStyles.subtext}>Voici notre sélection des meilleurs produits de cette semaine</Text>
+
+          {isLoading ? <ActivityIndicator /> : (
+            <ListedProducts products={data} />
+          )}
+        </View>
+
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default BestRated;
--- app/(tabs)/home/trends.js
+++ app/(tabs)/home/trends.js
@@ -1,26 +1,43 @@
-import { SafeAreaView, ScrollView, View, Text } from "react-native";
+import { SafeAreaView, ScrollView, View, Text, ActivityIndicator } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import commonStyles from "../../../styles/common";
+import ListedProducts from "../../../components/home/ListedProducts";
+import React, { useEffect, useState } from 'react';
 
 const Trends = () => {
-    const router = useRouter()
-  
-    return (
-      <View style={commonStyles.body}>
-        <SafeAreaView>
-          <Stack.Screen
-            options={{
-              headerStyle: commonStyles.header,
-              headerShadowVisible: false,
-              headerTitle: "",
-            }}
-          /> 
+  const router = useRouter();
 
-          <Text>TRENDS ECRAN OUI</Text>
+  const api = require('../../../api/api');
 
-        </SafeAreaView>
-      </View>
-    );
-  };
-  
-  export default Trends;
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    api.getTrendsFromApi(setData, setLoading);
+  }, []);
+
+  return (
+    <View style={commonStyles.body}>
+      <SafeAreaView style={commonStyles.flexSafeArea}>
+        <Stack.Screen
+          options={{
+            headerStyle: commonStyles.header,
+            headerShadowVisible: false,
+            headerTitle: "",
+          }}
+        />
+        <View style={commonStyles.flexContainer}>
+          <Text style={commonStyles.heading}>Les plus tendances</Text>
+          <Text style={commonStyles.subtext}>Voici notre sélection des produits les plus scannés de cette semaine</Text>
+
+          {isLoading ? <ActivityIndicator /> : (
+            <ListedProducts products={data} />
+          )}
+        </View>
+
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default Trends;

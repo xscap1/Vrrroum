2586a51144a556aa580e3e34ab786c5a2b9e7b9a
xscap1 <poujoltheo@gmail.com>
2024-02-13 16:28:32 +0100

    [api] Added api calls

---
+++ api/api.js
@@ -0,0 +1,49 @@
+const getBestRatedPreviewFromApi = async (setData, setLoading) => {
+    try {
+        await fetch('http://localhost:8383/api/bestRatedPreview')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getBestRatedFromApi = async (setData, setLoading) => {
+    try {
+        await fetch('http://localhost:8383/api/bestRated')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getTrendsPreviewFromApi = async (setData, setLoading) => {
+    try {
+        await fetch('http://localhost:8383/api/trendsPreview')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getTrendsFromApi = async (setData, setLoading) => {
+    try {
+        await fetch('http://localhost:8383/api/trends')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export { getBestRatedFromApi, getBestRatedPreviewFromApi, getTrendsFromApi, getTrendsPreviewFromApi }

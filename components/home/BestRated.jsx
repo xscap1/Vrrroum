636553a2153ae4412dd51cf85882643b800958fd
xscap1 <poujoltheo@gmail.com>
2024-02-13 16:29:40 +0100

    [component] Make api calls and display result in BestRated and Trendings component

--- components/home/BestRated.jsx
+++ components/home/BestRated.jsx
@@ -1,37 +1,44 @@
-import React from "react";
 import { useRouter } from "expo-router";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import PreviewCard from "../common/cards/previewCard/PreviewCard";
 import commonStyles from "../../styles/common";
 import SeeMoreButton from "../common/buttons/SeeMoreButton";
-import {COLORS, SIZES } from "../../constants"
+import { COLORS, SIZES } from "../../constants";
+import React, { useEffect, useState } from 'react';
 
 const BestRated = () => {
     const router = useRouter();
-    
-    const data = [1, 2, 3];
-
     const utils = require('../../constants/utils');
+    const api = require('../../api/api');
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        api.getBestRatedPreviewFromApi(setData, setLoading);
+    }, []);
 
     return (
         <View style={commonStyles.container}>
             <Text style={commonStyles.heading}>Les mieux notés</Text>
-            <View style={{marginTop: 15}}>
-                <FlatList
-                 data={data}
-                 renderItem={({ item }) => (
-                    <PreviewCard
-                        product={item}
-                        colorNote={utils.noteToColor(item)}
+            <View style={{ marginTop: 15 }}>
+                {isLoading ? <ActivityIndicator /> : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }) => id}
+                        renderItem={({ item }) => (
+                            <PreviewCard
+                                product={item}
+                                colorNote={utils.noteToColor(item.score)}
+                            />
+                        )}
+                        contentContainerStyle={{ columnGap: SIZES.medium }}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
                     />
-                 )}
-                 keyExtractor={(item) => item}
-                 contentContainerStyle={{ columnGap: SIZES.medium }}
-                 horizontal
-                 showsHorizontalScrollIndicator={false}
-            />
+                )}
             </View>
-            <SeeMoreButton handlePress={() => {router.push(`/home/bestRated`)}}/>
+            <SeeMoreButton handlePress={() => { router.push(`/home/bestRated`) }} />
         </View>
     );
 };
--- components/home/Trendings.jsx
+++ components/home/Trendings.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { useRouter } from "expo-router";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, ActivityIndicator, TouchableOpacity } from "react-native";
 import PreviewCard from "../common/cards/previewCard/PreviewCard";
 import commonStyles from "../../styles/common";
 import SeeMoreButton from "../common/buttons/SeeMoreButton";
@@ -8,30 +8,37 @@ import {COLORS, SIZES } from "../../constants"
 
 const Trendings = () => {
     const router = useRouter();
-    
-    const data = [1, 2, 3];
-
     const utils = require('../../constants/utils');
+    const api = require('../../api/api');
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        api.getTrendsPreviewFromApi(setData, setLoading);
+    }, []);
 
     return (
         <View style={commonStyles.container}>
-            <Text style={commonStyles.heading}>Les tendances</Text>
-            <View style={{marginTop: 15}}>
-                <FlatList
-                 data={data}
-                 renderItem={({ item }) => (
-                    <PreviewCard
-                        product={item}
-                        colorNote={utils.noteToColor(item)}
+            <Text style={commonStyles.heading}>Les mieux notés</Text>
+            <View style={{ marginTop: 15 }}>
+                {isLoading ? <ActivityIndicator /> : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }) => id}
+                        renderItem={({ item }) => (
+                            <PreviewCard
+                                product={item}
+                                colorNote={utils.noteToColor(item.score)}
+                            />
+                        )}
+                        contentContainerStyle={{ columnGap: SIZES.medium }}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
                     />
-                 )}
-                 keyExtractor={(item) => item}
-                 contentContainerStyle={{ columnGap: SIZES.medium }}
-                 horizontal
-                 showsHorizontalScrollIndicator={false}
-            />
+                )}
             </View>
-            <SeeMoreButton handlePress={() => {router.push(`/home/trends`)}}/>
+            <SeeMoreButton handlePress={() => { router.push(`/home/trends`) }} />
         </View>
     );
 };

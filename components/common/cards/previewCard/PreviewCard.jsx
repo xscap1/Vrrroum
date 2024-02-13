11cc50ff6d56b2e88af1cbacce7f2d411e42baa9
xscap1 <poujoltheo@gmail.com>
2024-02-13 16:29:12 +0100

    [component] Display props properties in PreviewCard

--- components/common/cards/previewCard/PreviewCard.jsx
+++ components/common/cards/previewCard/PreviewCard.jsx
@@ -5,44 +5,47 @@ import styles from "./previewCardStyles";
 import { COLORS, SIZES } from "../../../../constants"
 
 const PreviewCard = ({ product, colorNote }) => {
+
     return (
         <View style={styles.wrapper}>
-            <View style={{ height: '70%'}}>
-                <Image
-                    source={{
-                        uri: "https://s1.medias-norauto.fr/images_produits/70382800475/400x400/nettoyant-jantes-hot-rims-meguiar-s-710-ml--356783.jpg"
-                    }}
-                    style={styles.img}
-                />
-            </View>
-
-            <View>
-                <View style={styles.categoryBrandWrapper}>
-
-                    <View style={styles.brandWrap}>
-                        <Text style={styles.brand}>Marque</Text>
-                    </View>
-
-                    <View style={styles.categoryWrap}>
-                        <Text style={styles.brand}>Catégorie</Text>
-                    </View>
-
+            {product ? (
+                <View style={{ height: '70%' }}>
+                    <Image
+                        source={{
+                            uri: product.img
+                        }}
+                        style={styles.img}
+                    />
                 </View>
+            ) : null}
 
-                <View style={styles.categoryBrandWrapper}>
-                    <View style={styles.nameWrap}>
-                        <View style={{width: 'auto', height: 40, justifyContent: 'center'}}>
-                            <Text style={styles.name}>Hot Rims</Text>
+            {product ? (
+                <View>
+                    <View style={styles.categoryBrandWrapper}>
+                        <View style={styles.brandWrap}>
+                            <Text style={styles.brand}>{product.brand}</Text>
+                        </View>
+
+                        <View style={styles.categoryWrap}>
+                            <Text style={styles.brand}>{product.category}</Text>
                         </View>
                     </View>
 
-                    <View style={styles.noteWrap}>
-                        <View style={{ borderRadius: 100, width: 40, height: 40, justifyContent: 'center', backgroundColor: colorNote }}>
-                            <Text style={commonStyles.noteText}>8</Text>
+                    <View style={styles.categoryBrandWrapper}>
+                        <View style={styles.nameWrap}>
+                            <View style={{ width: 'auto', height: 40, justifyContent: 'center' }}>
+                                <Text style={styles.name}>{product.name}</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.noteWrap}>
+                            <View style={{ borderRadius: 100, width: 40, height: 40, justifyContent: 'center', backgroundColor: colorNote }}>
+                                <Text style={commonStyles.noteText}>{product.score}</Text>
+                            </View>
                         </View>
                     </View>
                 </View>
-            </View>
+            ) : null}
 
 
         </View>

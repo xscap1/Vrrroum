e41398275631779d3b12cc9594dce20b53acfacc
xscap1 <poujoltheo@gmail.com>
2024-02-19 16:15:12 +0100

    [app] Added category screen

---
+++ app/(tabs)/home/category.js
@@ -0,0 +1,58 @@
+import { SafeAreaView, ScrollView, View, Text, ActivityIndicator } from "react-native";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import commonStyles from "../../../styles/common";
+import ListedProducts from "../../../components/home/ListedProducts";
+import React, { useEffect, useState } from 'react';
+
+const Category = () => {
+    const router = useRouter();
+
+    const api = require('../../../api/api');
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState();
+    const [cursor, setCursor] = useState(null);
+
+    const local = useLocalSearchParams();
+
+    const category = local.category;
+
+    useEffect(() => {
+        api.geCategoryBatchFromApi(category, null, setData, setLoading, setCursor);
+        console.log(cursor);
+    }, []);
+
+
+    const fetchData = () => {
+        try {
+            api.geCategoryBatchFromApi(category, cursor, setData, setLoading, setCursor);
+        } catch (error) {
+            console.error('Erreur lors du chargement des données:', error);
+        }
+    };
+
+
+    return (
+        <View style={commonStyles.body}>
+            <SafeAreaView style={commonStyles.flexSafeArea}>
+                <Stack.Screen
+                    options={{
+                        headerStyle: commonStyles.header,
+                        headerShadowVisible: false,
+                        headerTitle: "",
+                    }}
+                />
+                <View style={commonStyles.flexContainer}>
+                    <Text style={commonStyles.heading}>{category}</Text>
+
+                    {isLoading ? <ActivityIndicator /> : (
+                        <ListedProducts products={data} onEndOnPress={fetchData}/>
+                    )}
+                </View>
+
+            </SafeAreaView>
+        </View>
+    );
+};
+
+export default Category;

122cc5f994c2adc7a355dee24e5fdf8180b6b8e6
xscap1 <poujoltheo@gmail.com>
2024-02-29 16:35:53 +0100

    [component] Added flatlist optional view in ListedProduct component

--- components/home/ListedProducts.jsx
+++ components/home/ListedProducts.jsx
@@ -4,31 +4,41 @@ import { View, Text, FlatList } from "react-native";
 import ListedProductCard from "../common/cards/listedProduct/ListedProductCard";
 import { COLORS, SIZES } from "../../constants"
 
-const ListedProducts = ({ products, scan, onEndOnPress }) => {
+const ListedProducts = ({ products, scan, onEndOnPress, flatlist = true }) => {
     const router = useRouter();
 
     const utils = require('../../constants/utils');
 
     return (
         <View style={{ flex: 1 }}>
-            <View style={{ marginTop: 15, flex: 1 }}>
-                <FlatList
-                    style={{ flex: 1 }}
-                    data={products}
-                    keyExtractor={({ id }) => id}
-                    renderItem={({ item }) => (
-                        <ListedProductCard
-                            product={item}
-                            colorNote={utils.noteToColor(item.score)}
-                            scan = {scan}
-                        />
-                    )}
-                    contentContainerStyle={{ columnGap: SIZES.medium }}
-                    showsVerticalScrollIndicator={false}
-                    onEndReached={() => { if (onEndOnPress != null) onEndOnPress(); }}
-                    onEndReachedThreshold={0.2}
-                />
-            </View>
+            {products ? (
+                <View style={{ marginTop: 15, flex: 1 }}>
+                    {flatlist ?
+                        <FlatList
+                            style={{ flex: 1 }}
+                            data={products}
+                            keyExtractor={({ id }) => id}
+                            renderItem={({ item }) => (
+                                <ListedProductCard
+                                    product={item}
+                                    colorNote={utils.noteToColor(item.score)}
+                                    scan={scan}
+                                />
+                            )}
+                            contentContainerStyle={{ columnGap: SIZES.medium }}
+                            showsVerticalScrollIndicator={false}
+                            onEndReached={() => { if (onEndOnPress != null) onEndOnPress(); }}
+                            onEndReachedThreshold={0.2}
+                        /> :
+                        products.map(function (item, i) {
+                            return <ListedProductCard
+                                key={i}
+                                product={item}
+                                colorNote={utils.noteToColor(item.score)}
+                                scan={scan}
+                            />
+                        })}
+                </View>) : null}
         </View>
     );
 };

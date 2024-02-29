a8d67d6ecf762d54ba759a111566cbfbf6cbe163
xscap1 <poujoltheo@gmail.com>
2024-02-29 17:40:35 +0100

    [app] Remove all previous views to only add single component in Product screen

--- app/(tabs)/home/product.js
+++ app/(tabs)/home/product.js
@@ -1,30 +1,15 @@
-import { SafeAreaView, ScrollView, View, Text, ActivityIndicator, TouchableOpacity } from "react-native";
+import { SafeAreaView, View } from "react-native";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import commonStyles from "../../../styles/common";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ProductCard from "../../../components/common/cards/Product/productCard";
-import Details from "../../../components/common/products/details/details";
-import { COLORS } from '../../../constants';
-import ListedProducts from "../../../components/home/ListedProducts";
+
 
 const Product = () => {
+
     const router = useRouter();
-
     const { product } = useLocalSearchParams();
-
     const utils = require('../../../constants/utils');
-
-    const [show, setShow] = useState(true);
-
-    const api = require('../../../api/api');
-
-    const [isLoading, setLoading] = useState(true);
-    const [recommendations, setRecommendations] = useState();
-
-    useEffect(() => {
-        // api.getRecommendationsFromApi(product.id, product.category, product.score, setRecommendations, setLoading);
-        api.getRecommendationsFromApi('70382800598', 'interior_plastic_cleaner', 7.2, setRecommendations, setLoading);
-    }, []);
 
     return (
         <View style={commonStyles.body}>
@@ -36,56 +21,7 @@ const Product = () => {
                         headerTitle: "",
                     }}
                 />
-                <View style={commonStyles.flexContainer}>
-                    <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
-                        <ProductCard product={product} colorNote={utils.noteToColor(product.score)} />
-
-                        <View style={{ flexDirection: 'row', marginTop: '10%' }}>
-                            <View style={{ width: '50%' }}>
-                                <TouchableOpacity style={{ alignSelf: 'center', width: '80%' }} disabled={show} onPress={() => setShow(!show)}>
-                                    <View style={show ? commonStyles.dataActiveButton : commonStyles.dataInactiveButton}>
-                                        <Text style={show ? commonStyles.activeText : commonStyles.inactiveText}>Informations</Text>
-                                    </View>
-                                </TouchableOpacity>
-                            </View>
-                            <View style={{ width: '50%' }}>
-                                <TouchableOpacity style={{ alignSelf: 'center', width: '80%' }} disabled={!show} onPress={() => setShow(!show)}>
-                                    <View style={!show ? commonStyles.dataActiveButton : commonStyles.dataInactiveButton}>
-                                        <Text style={!show ? commonStyles.activeText : commonStyles.inactiveText}>Environnement</Text>
-                                    </View>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-
-                        {show ?
-                            <View>
-                                <Details product={product} />
-                            </View>
-                            : null}
-
-                        {!show ?
-                            <View>
-                                <Text style={commonStyles.text}>Env</Text>
-                            </View>
-                            : null}
-
-                        <View style={{ marginTop: 20 }}>
-                            <Text style={commonStyles.heading}>Comparateur de prix</Text>
-                            <View style={commonStyles.subcontainer}>
-                                <Text style={{ color: COLORS.subwhite, alignSelf: 'center', textAlign: 'center' }}>Cette fonctionnalité sera bientôt disponible !
-                                    L'équipe Vrrroum travaille dur pour vous offrir la solution d'achat en ligne la plus économique.</Text>
-                            </View>
-                        </View>
-
-                        <View style={{ marginTop: 20 }}>
-                            <Text style={commonStyles.heading}>Recommendations</Text>
-                            {isLoading ? <ActivityIndicator /> : <ListedProducts products={recommendations} flatlist={false} />}
-                        </View>
-                    </ScrollView>
-
-
-                </View>
-
+                <ProductCard product={product} colorNote={utils.noteToColor(product.score)} />
             </SafeAreaView>
         </View>
     );

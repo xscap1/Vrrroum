129d0e546085bee1f853b2466cc0289bf6375e39
xscap1 <poujoltheo@gmail.com>
2024-03-05 16:47:02 +0100

    [component] ListedProductCard component is now clickable to display product card data

--- components/common/cards/listedProduct/ListedProductCard.jsx
+++ components/common/cards/listedProduct/ListedProductCard.jsx
@@ -3,75 +3,82 @@ import { View, Text, Image, TouchableOpacity } from "react-native";
 import commonStyles from "../../../../styles/common";
 import styles from "./listedProductCardStyles";
 import { COLORS, SIZES, icons } from "../../../../constants"
+import { useNavigation } from "expo-router";
 
 const ListedProductCard = ({ product, colorNote, scan }) => {
 
     const wrapperStyle = product.isSponso == true ? styles.wrapperSponso : styles.wrapper;
+    const navigation = useNavigation();
 
     return (
-        <View>
-            <View style={commonStyles.subcontainer}>
-                {product ? (
-                    <View style={wrapperStyle}>
-                        <View style={styles.imgContiner}>
-                            <Image
-                                source={{
-                                    uri: product.img == null ? product.imgUrl : product.img
-                                }}
-                                style={styles.img}
-                            />
-                        </View>
-                        <View style={styles.datasContainer}>
-                            {product.isSponso == true ?
+        <TouchableOpacity onPress={() => {
+            navigation.push('product', {
+                product: product
+            })
+        }}>
+            <View>
+                <View style={commonStyles.subcontainer}>
+                    {product ? (
+                        <View style={wrapperStyle}>
+                            <View style={styles.imgContiner}>
+                                <Image
+                                    source={{
+                                        uri: product.img == null ? product.imgUrl : product.img
+                                    }}
+                                    style={styles.img}
+                                />
+                            </View>
+                            <View style={styles.datasContainer}>
+                                {product.isSponso == true ?
+                                    <View>
+                                        <Text style={{
+                                            fontSize: SIZES.small,
+                                            color: COLORS.subwhite,
+                                            marginBottom: 5
+                                        }}>Sponsorisé</Text>
+                                    </View> : null}
                                 <View>
-                                    <Text style={{
-                                        fontSize: SIZES.small,
-                                        color: COLORS.subwhite,
-                                        marginBottom: 5
-                                    }}>Sponsorisé</Text>
-                                </View> : null}
-                            <View>
-                            {/* backgroundColor: COLORS.background, borderRadius: 100, borderWidth: 1, borderColor: COLORS.yellow,  */}
-                                <View>
-                                    <Text numberOfLines={2} style={{ color: COLORS.lightwhite, fontWeight: '500' }}>{product.name}</Text>
-                                </View>
-                                <View style={{ marginTop: 10, flexDirection: 'row' }}>
-                                    <View style={{ alignSelf: 'flex-start' }}>
-                                        <Text style={{ color: COLORS.yellow, padding: 5 }}>{product.brand}</Text>
+                                    {/* backgroundColor: COLORS.background, borderRadius: 100, borderWidth: 1, borderColor: COLORS.yellow,  */}
+                                    <View>
+                                        <Text numberOfLines={2} style={{ color: COLORS.lightwhite, fontWeight: '500' }}>{product.name}</Text>
                                     </View>
-                                    <View style={{ alignSelf: 'center', marginLeft: 10 }}>
-                                        <Text style={{ color: COLORS.subwhite }}>{product.category}</Text>
+                                    <View style={{ marginTop: 10, flexDirection: 'row' }}>
+                                        <View style={{ alignSelf: 'flex-start' }}>
+                                            <Text style={{ color: COLORS.yellow, padding: 5 }}>{product.brand}</Text>
+                                        </View>
+                                        <View style={{ alignSelf: 'center', marginLeft: 10 }}>
+                                            <Text style={{ color: COLORS.subwhite }}>{product.category}</Text>
+                                        </View>
                                     </View>
-                                </View>
-                                <View style={{ flexDirection: 'row', alignSelf: 'flex-end', alignItems: 'center' }}>
-                                    {scan ? (
-                                        <View style={commonStyles.scanContainer}>
-                                            <View>
-                                                <Text style={commonStyles.scan}>{product.scans}</Text>
-                                            </View>
-                                            <View>
-                                                <Image
-                                                    source={icons.flame}
-                                                    style={styles.icon}
-                                                />
-                                            </View>
-                                        </View>) : null}
+                                    <View style={{ flexDirection: 'row', alignSelf: 'flex-end', alignItems: 'center' }}>
+                                        {scan ? (
+                                            <View style={commonStyles.scanContainer}>
+                                                <View>
+                                                    <Text style={commonStyles.scan}>{product.scans}</Text>
+                                                </View>
+                                                <View>
+                                                    <Image
+                                                        source={icons.flame}
+                                                        style={styles.icon}
+                                                    />
+                                                </View>
+                                            </View>) : null}
 
-                                    <View style={{ marginLeft: 20 }}>
-                                        <View style={{ borderRadius: 100, width: 40, height: 40, justifyContent: 'center', backgroundColor: COLORS.background, borderWidth: '1', borderStyle: 'solid', borderColor: colorNote }}>
-                                            <Text style={{
-                                                textAlign: 'center',
-                                                fontWeight: '500',
-                                                fontSize: SIZES.medium,
-                                                marginTop: 0,
-                                                width: 40,
-                                                color: colorNote
-                                            }}>{product.score}</Text>
+                                        <View style={{ marginLeft: 20 }}>
+                                            <View style={{ borderRadius: 100, width: 40, height: 40, justifyContent: 'center', backgroundColor: COLORS.background, borderWidth: '1', borderStyle: 'solid', borderColor: colorNote }}>
+                                                <Text style={{
+                                                    textAlign: 'center',
+                                                    fontWeight: '500',
+                                                    fontSize: SIZES.medium,
+                                                    marginTop: 0,
+                                                    width: 40,
+                                                    color: colorNote
+                                                }}>{product.score}</Text>
+                                            </View>
                                         </View>
                                     </View>
                                 </View>
-                            </View>
-                            {/* <View style={{ flexDirection: 'row', justifyContent: 'space-between', gap: 10, marginBottom: 2 }}>
+                                {/* <View style={{ flexDirection: 'row', justifyContent: 'space-between', gap: 10, marginBottom: 2 }}>
                                 <View style={{ paddingVertical: 5 }}>
                                     <Text style={styles.branded}> {product.brand}</Text>
                                 </View>
@@ -90,11 +97,12 @@ const ListedProductCard = ({ product, colorNote, scan }) => {
                                     }}>{product.score}</Text>
                                 </View>
                             </View> */}
+                            </View>
                         </View>
-                    </View>
-                ) : null}
+                    ) : null}
+                </View>
             </View>
-        </View>
+        </TouchableOpacity>
     );
 };
 

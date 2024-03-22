14cc10149e9d01cde6cf858c2b659efc175b45d8
xscap1 <poujoltheo@gmail.com>
2024-03-22 19:26:52 +0100

    [app] A bit of clean in home screen

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView, ScrollView, View, Text, Platform } from "react-native";
 import { Stack, useRouter, Link } from "expo-router";
 import Trendings from "../../../components/home/Trendings";
@@ -11,48 +11,29 @@ import { SearchBar } from '@rneui/themed';
 import { COLORS } from "../../../constants";
 
 const Home = () => {
-    const router = useRouter()
 
-    const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
 
-    const updateSearch = (search) => {
-      setSearch(search);
-    };
-
-    const product1 = {
-      brand: 'Meguiar\'s',
-      name: 'Quick detailler', 
-      category: 'Nettoyant jantes',
-      isSponso: true,
-      note: 2.5
-    }
-
-    const product2 = {
-      brand: 'Total',
-      name: 'Quarts 9000 5W40', 
-      category: 'Huile moteur',
-      isSponso: false,
-      note: 9
-    }
-
-    return (
+  return (
       <View style={commonStyles.body}>
         <SafeAreaView style={commonStyles.safeArea}>
           <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={{paddingLeft: 0.02 * ww, paddingRight: 0.02 * ww,}}>
+            <View style={{ paddingLeft: 0.02 * ww, paddingRight: 0.02 * ww, }}>
               <SearchBar
-                  placeholder="Rechercher un produit"
-                  onChangeText={updateSearch}
-                  value={search}
-                  platform={Platform.OS}
-                  searchIcon={null}
-                  containerStyle={{backgroundColor: COLORS.background}}
-                  inputContainerStyle={{backgroundColor: COLORS.darkgray, borderRadius: 10}}
-                  inputStyle={{backgroundColor: COLORS.darkgray, color: COLORS.lightwhite, paddingLeft: 10}}
-                  onFocus={() => {}}
+                placeholder="Rechercher un produit"
+                onChangeText={updateSearch}
+                value={search}
+                platform={Platform.OS}
+                searchIcon={null}
+                containerStyle={{ backgroundColor: COLORS.background }}
+                inputContainerStyle={{ backgroundColor: COLORS.darkgray, borderRadius: 10 }}
+                inputStyle={{ backgroundColor: COLORS.darkgray, color: COLORS.lightwhite, paddingLeft: 10 }}
+                onFocus={() => { }}
               />
             </View>
-            {/* <ListedProducts products={[product1, product2]}/> */}
             <Offers />
             <Trendings />
             <BestRated />
@@ -60,7 +41,7 @@ const Home = () => {
           </ScrollView>
         </SafeAreaView>
       </View>
-    );
-  };
-  
-  export default Home;
+  );
+};
+
+export default Home;

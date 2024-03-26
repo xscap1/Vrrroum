ebffc8d09f3589a3d7ef53b6abeb4709d9a6a3b3
xscap1 <poujoltheo@gmail.com>
2024-03-26 17:29:19 +0100

    [component] Created listed button component for profile and other screens

---
+++ components/common/buttons/ListedButton.jsx
@@ -0,0 +1,46 @@
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import styles from "./seemoreButtonsStyles";
+import { COLORS, SIZES } from "../../../constants";
+import { StyleSheet } from "react-native-web";
+
+const ListedButton = ({ text, handlePress, disabled }) => {
+
+    var styles = StyleSheet.create({
+        btnContainer: {
+            display: 'flex', 
+            flexDirection: 'row', 
+            backgroundColor: COLORS.lightgray, 
+            padding: 15, 
+            borderRadius: 15, 
+            justifyContent: 'space-between'
+        },
+    
+        disabledBtnContainer: {
+            display: 'flex', 
+            flexDirection: 'row', 
+            backgroundColor: COLORS.darkgray, 
+            padding: 15, 
+            borderRadius: 15, 
+            justifyContent: 'space-between'
+        },
+    
+        text: {
+            color: COLORS.lightwhite, 
+            fontSize: 16
+        },
+    
+        disabledText: {
+            color: COLORS.lightgray, 
+            fontSize: 16
+        }
+    });
+
+    return (
+        <TouchableOpacity disabled={disabled} onPress={handlePress} style={ disabled ? styles.disabledBtnContainer : styles.btnContainer }>
+            <Text style={disabled ? styles.disabledText : styles.text}>{text}</Text>
+            <Text style={disabled ? styles.disabledText : styles.text}>{'\>'}</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default ListedButton;

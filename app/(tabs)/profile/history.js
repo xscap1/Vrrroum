804d3d58d5e624b1241c3812d2cdf1a3a0e533b5
xscap1 <poujoltheo@gmail.com>
2024-04-04 17:24:42 +0200

    [app] Add history screen

--- app/(tabs)/profile/history.js
+++ app/(tabs)/profile/history.js
@@ -2,10 +2,25 @@ import { SafeAreaView, ScrollView, View, Text, ActivityIndicator } from "react-n
 import { Stack, useRouter } from "expo-router";
 import commonStyles from "../../../styles/common";
 import React, { useEffect, useState } from 'react';
+import ListedProducts from "../../../components/home/ListedProducts";
+import { deleteHistory, getHistoryInCache, storeHistoryInCache } from "../../../utils";
 
 const History = () => {
 
+  const api = require('../../../api/api');
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState();
+
   useEffect(() => {
+    const getHistory = async () => {
+      // await deleteHistory();
+      const h = await getHistoryInCache();
+      if (h != null && h.length > 0)
+        api.PostIdsFromApi(JSON.stringify(h), setData, setLoading);
+
+    }
+
+    getHistory();
   }, []);
 
   return (
@@ -20,6 +35,9 @@ const History = () => {
         />
         <View style={commonStyles.flexContainer}>
           <Text style={commonStyles.heading}>Historique</Text>
+          {isLoading ? <ActivityIndicator /> : (
+            <ListedProducts products={data} />
+          )}
         </View>
       </SafeAreaView>
     </View>

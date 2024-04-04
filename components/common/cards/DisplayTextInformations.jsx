695233117524eb142d2f7f04e709c13f7cba7e98
xscap1 <poujoltheo@gmail.com>
2024-04-04 18:25:42 +0200

    [component] Created component to display some important text informations

---
+++ components/common/cards/DisplayTextInformations.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import { COLORS, SIZES } from "../../../constants";
+import commonStyles from "../../../styles/common";
+
+const DisplayTextInformations = ({ title, text }) => {
+
+    return (
+        <View style={{ marginTop: 20 }}>
+            <Text style={commonStyles.heading}>{title}</Text>
+            <View style={commonStyles.subcontainer}>
+                <Text style={{ color: COLORS.subwhite, alignSelf: 'center', textAlign: 'center' }}>{text}</Text>
+            </View>
+        </View>
+    );
+};
+
+export default DisplayTextInformations;

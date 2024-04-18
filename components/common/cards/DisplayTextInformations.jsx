32cdc43ac68d297917a01a7957d4325be76f4fb0
xscap1 <poujoltheo@gmail.com>
2024-04-18 15:10:31 +0200

    [component] Added component to display datas to user

--- components/common/cards/DisplayTextInformations.jsx
+++ components/common/cards/DisplayTextInformations.jsx
@@ -3,9 +3,8 @@ import { COLORS, SIZES } from "../../../constants";
 import commonStyles from "../../../styles/common";
 
 const DisplayTextInformations = ({ title, text }) => {
-
     return (
-        <View style={{ marginTop: 20 }}>
+        <View style={{marginTop: 20}}>
             <Text style={commonStyles.heading}>{title}</Text>
             <View style={commonStyles.subcontainer}>
                 <Text style={{ color: COLORS.subwhite, alignSelf: 'center', textAlign: 'center' }}>{text}</Text>

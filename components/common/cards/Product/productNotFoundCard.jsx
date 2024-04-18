3b4a52b2736021398362a56a29e85fd24f542634
xscap1 <poujoltheo@gmail.com>
2024-04-18 15:20:01 +0200

    [component] Display Report missing data component when no product found

--- components/common/cards/Product/productNotFoundCard.jsx
+++ components/common/cards/Product/productNotFoundCard.jsx
@@ -3,21 +3,28 @@ import { View, Text } from 'react-native';
 import commonStyles from "../../../../styles/common";
 import SeeMoreButton from "../../buttons/SeeMoreButton";
 import { useNavigation } from "expo-router";
+import ScreenHeaderBtn from '../../buttons/SeeMoreButton';
+import ReportMissingData from './reportMissingData';
 
-const ProductNotFoundCard = () => {
+const ProductNotFoundCard = (code) => {
 
     const navigation = useNavigation();
 
+    const [displayData, setDisplayData] = useState(false);
+
     return (
         <View style={commonStyles.flexContainer}>
-            <View style={commonStyles.subcontainer}>
-                <View style={{ alignItems: 'center' }}>
+            {displayData ? <ReportMissingData code={code.code} /> :
+                <View style={commonStyles.subcontainer}><View style={{ alignItems: 'center' }}>
                     <Text style={commonStyles.text}>Produit introuvable</Text>
                     <View style={{ marginTop: 10 }}>
-                        <Text style={commonStyles.subtextCenter}>Vous pourrez bientôt participer à l'agrandissement de Vrrroum en renseignant les données d'un produit manquant !</Text>
+                        <Text style={commonStyles.subtextCenter}>Vous pouvez contribuer à l'agrandissement de Vrrroum en renseignant les informations manquantes ! Assurez-vous que vos renseignements soient corrects.</Text>
+                        <ScreenHeaderBtn text={"Renseigner les informations"} handlePress={() => { setDisplayData(true); }} />
                     </View>
                 </View>
-            </View>
+                </View>}
+
+
         </View>);
 }
 

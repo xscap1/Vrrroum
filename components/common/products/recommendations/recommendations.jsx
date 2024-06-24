4996db3ae7956198ee89de3dcbd35a0166aabe74
xscap1 <poujoltheo@gmail.com>
2024-06-24 13:56:00 +0200

    [component] Display missing recommendations text in product card

--- components/common/products/recommendations/recommendations.jsx
+++ components/common/products/recommendations/recommendations.jsx
@@ -4,11 +4,12 @@ import commonStyles from "../../../../styles/common";
 import ListedProducts from "../../../home/ListedProducts";
 import NoAccess from "../../noaccess/NoAccess";
 import { isSubscriptionActiveFromRCProvider } from "../../../../utils/rcprovider";
+import DisplayTextInformations from "../../cards/DisplayTextInformations";
 
 const Recommendations = ({ product }) => {
 
     const [isLoading, setLoading] = useState(true);
-    const [recommendations, setRecommendations] = useState();
+    const [recommendations, setRecommendations] = useState([]);
     const [isMember, setIsMember] = useState();
 
     const api = require('../../../../api/api');
@@ -31,7 +32,16 @@ const Recommendations = ({ product }) => {
         <View>
             <Text style={commonStyles.heading}>Recommandations</Text>
             <View>
-                {isLoading ? <ActivityIndicator /> : <ListedProducts products={recommendations} flatlist={false} />}
+                {/* {isLoading ? <ActivityIndicator /> : <ListedProducts products={recommendations} flatlist={false} />} */}
+                {isLoading ? (
+                    <ActivityIndicator />
+                ) : (
+                    recommendations.length > 0 ? (
+                        <ListedProducts products={recommendations} flatlist={false} />
+                    ) : (
+                        <DisplayTextInformations text={"Aucune recommandations trouvées pour l'instant. L'équipe Vrrroum travaille pour vous recommander des produits similaires."} />
+                    )
+                )}
             </View>
         </View>
     );

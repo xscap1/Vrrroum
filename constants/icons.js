42a028ed9d7a9aac565e7a49a15092185e825e41
xscap1 <poujoltheo@gmail.com>
2024-07-11 18:30:35 +0200

    [assets] Export new icons

--- constants/icons.js
+++ constants/icons.js
@@ -1,9 +1,9 @@
-import home  from "../assets/icons/home.png"
-import home_selected  from "../assets/icons/home_selected.png"
-import barcode  from "../assets/icons/barcode.png"
-import barcode_selected  from "../assets/icons/barcode_selected.png"
-import user  from "../assets/icons/user.png"
-import user_selected  from "../assets/icons/user_selected.png"
+import home from "../assets/icons/home.png"
+import home_selected from "../assets/icons/home_selected.png"
+import barcode from "../assets/icons/barcode.png"
+import barcode_selected from "../assets/icons/barcode_selected.png"
+import user from "../assets/icons/user.png"
+import user_selected from "../assets/icons/user_selected.png"
 import motoroil from "../assets/icons/motor_oil.png"
 import motoroil_selected from "../assets/icons/motor_oil_selected.png"
 import car from "../assets/icons/car.png"
@@ -12,9 +12,20 @@ import rim from "../assets/icons/rim.png"
 import shampoo from "../assets/icons/shampoo.png"
 import wax from "../assets/icons/wax.png"
 import flame from "../assets/icons/flame.png"
+import edit_sub from "../assets/icons/edit_sub.png"
+import unlock from "../assets/icons/unlock.png"
+import edit_account from "../assets/icons/edit_account.png"
+import logout from "../assets/icons/logout.png"
+import search from "../assets/icons/search.png"
+import login from "../assets/icons/login.png"
+import help from "../assets/icons/help.png"
+import hatch from "../assets/icons/hatch.png"
+import bug from "../assets/icons/bug.png"
+import info from "../assets/icons/info.png"
 
-
-export default { motoroil, motoroil_selected,
-                 home, home_selected,
-                 barcode, barcode_selected,
-                 user, user_selected, car, interior, rim, shampoo, wax, flame};
+export default {
+    motoroil, motoroil_selected,
+    home, home_selected,
+    barcode, barcode_selected,
+    user, user_selected, car, interior, rim, shampoo, wax, flame, edit_sub, unlock, edit_account, logout, search, login, help, hatch, bug, info
+};

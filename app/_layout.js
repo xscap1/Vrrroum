77481d68d4a77e1fd67f79176abc6b7082012c0c
xscap1 <poujoltheo@gmail.com>
2024-07-11 18:34:27 +0200

    [prj] Wrapped main stack navigator with Auth Context Provider

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 import commonStyles from "../styles/common";
-
+import { AuthProvider } from "../components/auth/AuthContext";
 // export const unstable_settings = {
 //   // Ensure any route can link back to `/`
 //   initialRouteName: "(tabs)",
@@ -8,10 +8,12 @@ import commonStyles from "../styles/common";
 
 const StackLayout = () => {
   return (
-    <Stack initialRouteName="(tabs)" >
-      <Stack.Screen name="index" options={{ headerShown: false}}/>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
-    </Stack>
+    <AuthProvider>
+      <Stack initialRouteName="(tabs)" >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
   )
 };
 

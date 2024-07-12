c5be00a98dec749d42cb0edfc7f82c866e367775
xscap1 <poujoltheo@gmail.com>
2024-07-12 17:29:45 +0200

    [app] Update subcription manager screen

--- app/(tabs)/profile/subscriptionManager.js
+++ app/(tabs)/profile/subscriptionManager.js
@@ -1,42 +1,36 @@
 import { SafeAreaView, ScrollView, View, Text, ActivityIndicator } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import commonStyles from "../../../styles/common";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import DisplayTextInformations from "../../../components/common/cards/DisplayTextInformations";
-import { getActiveSubscriptionInfoFromRCProvider } from "../../../utils/rcprovider";
+import SubscriptionContext from "../../../components/sub/SubcriptionContext";
 import SManager from "../../../components/profile/subscriptionManager/SManager";
 
 const SubscriptionManager = () => {
 
-  const api = require('../../../api/api');
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState();
-  const missingSubscriptionText = "Vous n'êtes pas abonné aux services Vrrroum."
-
-  useEffect(() => {
-    const getSubscriptionInfo = async ()=> {
-      const info = await getActiveSubscriptionInfoFromRCProvider();
-      setData(info);
-    }
-
-    getSubscriptionInfo();
-  }, []);
+  const { subscription } = useContext(SubscriptionContext);
 
   return (
     <View style={commonStyles.body}>
-      <SafeAreaView style={commonStyles.flexSafeArea}>
-        <Stack.Screen
-          options={{
-            headerStyle: commonStyles.header,
-            headerShadowVisible: false,
-            headerTitle: "",
-          }}
-        />
-        <View style={commonStyles.flexContainer}>
-          <Text style={commonStyles.heading}>Gérer mon abonnement</Text>
-          {data != undefined && data != {} && data != null ? <SManager offer={data} /> : <DisplayTextInformations text={missingSubscriptionText} />}
-        </View>
-      </SafeAreaView>
+
+      {subscription ?
+        <SafeAreaView style={commonStyles.flexSafeArea}>
+          <Stack.Screen
+            options={{
+              headerStyle: commonStyles.header,
+              headerShadowVisible: false,
+              headerTitle: "",
+            }}
+          />
+          <View style={commonStyles.flexContainer}>
+            <Text style={commonStyles.heading}>Gérer mon abonnement</Text>
+            <SManager />
+          </View>
+        </SafeAreaView>
+        :
+        null
+      }
+
     </View>
   );
 };

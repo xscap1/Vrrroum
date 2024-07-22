7036a55106c4d1cdb4ed91ce34271a47fceff3d3
xscap1 <poujoltheo@gmail.com>
2024-07-22 21:25:18 +0200

    [assets] Imported logo

--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,6 @@
 import offer1 from "../assets/images/offers/offer_1.jpeg"
 import offer2 from "../assets/images/offers/offer_2.jpeg"
 import offer3 from "../assets/images/offers/offer_3.jpeg"
+import logo from "../assets/images/logo/vrrroum_logo.png"
 
-export default {offer1, offer2, offer3};
+export default {offer1, offer2, offer3, logo};

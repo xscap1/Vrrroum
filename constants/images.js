823f625c184e665a908eca3439d206634ac8cf62
xscap1 <poujoltheo@gmail.com>
2024-11-22 12:20:48 +0100

    [const] Added new images constants

--- constants/images.js
+++ constants/images.js
@@ -2,5 +2,6 @@ import offer1 from "../assets/images/offers/offer_1.jpeg"
 import offer2 from "../assets/images/offers/offer_2.jpeg"
 import offer3 from "../assets/images/offers/offer_3.jpeg"
 import logo from "../assets/images/logo/vrrroum_logo.png"
+import vrrroum_cropped from "../assets/images/logo/vrrroum_cropped.png"
 
-export default {offer1, offer2, offer3, logo};
+export default {offer1, offer2, offer3, logo, vrrroum_cropped};

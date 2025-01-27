493fc8748d37a67d535a0f16e02b5ffe2f62619c
xscap1 <poujoltheo@gmail.com>
2025-01-27 20:12:06 +0100

    [component] Update text color in display text informations

--- components/common/cards/DisplayTextInformations.jsx
+++ components/common/cards/DisplayTextInformations.jsx
@@ -7,7 +7,7 @@ const DisplayTextInformations = ({ title, text }) => {
         <View style={{ marginTop: 0, marginBottom: 20 }}>
             <Text style={commonStyles.heading}>{title}</Text>
             <View style={commonStyles.subcontainer}>
-                <Text style={{ color: COLORS.subwhite, alignSelf: 'center', textAlign: 'center' }}>{text}</Text>
+                <Text style={{ color: COLORS.mainText, alignSelf: 'center', textAlign: 'center' }}>{text}</Text>
             </View>
         </View>
     );
